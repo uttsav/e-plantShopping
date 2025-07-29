f350fc805891729a25c8f09cff0060d995f8e5c9
Utsav Oza <utsav.oza@kennametal.com>
2025-07-29 17:21:48 +0530

    For deploying

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/e-plantShopping/', 
+  base: '/e-plantShopping/', // required for GitHub Pages
+  build: {
+    outDir: 'docs', // 👈 this is key
+  },
   plugins: [react()],
-})
+});
